18ee2da345a8afeb28f5a388e0ce23a586b2f985
ainembabaziderrick <ceoainembabaziderrick@gmail.com>
2025-02-27 15:23:32 +0300

    form changes

--- lib/raffley_web/live/raffle_live/index.ex
+++ lib/raffley_web/live/raffle_live/index.ex
@@ -6,18 +6,20 @@ defmodule RaffleyWeb.RaffleLive.Index do
 
   def mount(_params, _session, socket) do
     socket =
-      socket |>
-      stream(:raffles, Raffles.list_raffles()) |>
-      assign(:form, to_form%{})
+      socket
+      |> stream(:raffles, Raffles.list_raffles())
+      |> assign(:form, to_form(%{}))
+
     IO.inspect(socket.assigns.streams.raffles, label: "MOUNT")
+
     socket =
       attach_hook(socket, :log_stream, :after_render, fn
         socket ->
-        IO.inspect(socket.assigns.streams.raffles, label: "AFTER RENDER")
-        socket
+          IO.inspect(socket.assigns.streams.raffles, label: "AFTER RENDER")
+          socket
       end)
-    {:ok, socket}
 
+    {:ok, socket}
   end
 
   def render(assigns) do
@@ -26,16 +28,16 @@ defmodule RaffleyWeb.RaffleLive.Index do
       <.banner :if={false}>
         <.icon name="hero-sparkles-solid" /> Mystery Raffle Coming Soon!
         <:details :let={vibe}>
-          To Be Revealed Tomorrow <%= vibe %>
+          To Be Revealed Tomorrow {vibe}
         </:details>
         <:details>
           Any guesses?
         </:details>
       </.banner>
-      <.filter_form form={@form}/>
+      <.filter_form form={@form} />
 
-      <div class="raffles" id = "raffles" phx-update="stream">
-        <.raffle_card :for={{dom_id,raffle} <- @streams.raffles} raffle={raffle} id = {dom_id} />
+      <div class="raffles" id="raffles" phx-update="stream">
+        <.raffle_card :for={{dom_id, raffle} <- @streams.raffles} raffle={raffle} id={dom_id} />
       </div>
     </div>
     """
@@ -43,21 +45,21 @@ defmodule RaffleyWeb.RaffleLive.Index do
 
   def filter_form(assigns) do
     ~H"""
-    <.form for={@form}>
-        <.input field={@form[:q]} placeholder="Search..." autocomplete = "off"/>
-        <.input
-        field = {@form[:status]}
-        type = "select"
-        options = {[:upcoming, :open, :closed]}
-        prompt = "Status"
-        />
-        <.input
-        field = {@form[:sort_by]}
-        type = "select"
-        options = {[:prize, :ticket_price]}
-        prompt = "Sort By"
-        />
-      </.form>
+    <.form for={@form} id="filter-form" phx-submit="filter" phx-change="filter">
+      <.input field={@form[:q]} placeholder="Search..." autocomplete="off" />
+      <.input
+        field={@form[:status]}
+        type="select"
+        options={[:upcoming, :open, :closed]}
+        prompt="Status"
+      />
+      <.input
+        field={@form[:sort_by]}
+        type="select"
+        options={[:prize, :ticket_price]}
+        prompt="Sort By"
+      />
+    </.form>
     """
   end
 
@@ -67,17 +69,25 @@ defmodule RaffleyWeb.RaffleLive.Index do
   def raffle_card(assigns) do
     ~H"""
     <.link navigate={~p"/raffles/#{@raffle}"} id={@id}>
-    <div class="card">
-      <img src={@raffle.image_path} />
-      <h2><%= @raffle.prize %></h2>
-      <div class="details">
-        <div class="price">
-          $<%= @raffle.ticket_price %> / ticket
+      <div class="card">
+        <img src={@raffle.image_path} />
+        <h2>{@raffle.prize}</h2>
+        <div class="details">
+          <div class="price">
+            ${@raffle.ticket_price} / ticket
+          </div>
+          <.badge status={@raffle.status} />
         </div>
-        <.badge status={@raffle.status} />
       </div>
-    </div>
     </.link>
     """
+  end
+
+  def handle_event("filter", params, socket) do
+    socket =
+      socket |>
+      assign(:form, to_form(params)) |>
+      stream(:raffles, Raffles.filter_raffles(params), reset: true)
+    {:noreply, socket}
   end
 end
