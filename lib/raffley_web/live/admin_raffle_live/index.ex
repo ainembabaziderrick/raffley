8bb8cdd4f61f92249e9a285536a843d291b4b6ee
ainembabaziderrick <ceoainembabaziderrick@gmail.com>
2025-03-04 22:38:25 +0300

    JS commands

--- lib/raffley_web/live/admin_raffle_live/index.ex
+++ lib/raffley_web/live/admin_raffle_live/index.ex
@@ -16,33 +16,46 @@ defmodule RaffleyWeb.AdminRaffleLive.Index do
   def render(assigns) do
     ~H"""
     <div class="admin_index">
-      <.header>
+      <.button phx-click={
+        JS.toggle(
+          to: "#joke",
+          in: {"ease-in-out duration-300", "opacity-0", "opacity-100"},
+          out: {"ease-in-out duration-300", "opacity-100", "opacity-0"},
+          time: 300
+        )
+      }>
+        Toggle Joke
+      </.button>
+      <div id="joke" class="joke">
+        What is a tree's favourite drink?
+      </div>
+      <.header class="mt-6">
         {@page_title}
-        <:actions >
+        <:actions>
           <.link class="button" navigate={~p"/admin/raffles/new"}>New Raffle</.link>
         </:actions>
       </.header>
-      <.table id="raffles" rows={@streams.raffles}>
+      <.table id="raffles" rows={@streams.raffles} row_click={fn {_, raffle} -> JS.navigate(~p"/raffles/#{raffle}") end }>
         <:col :let={{_dom_id, raffle}} label="Prize">
           <.link navigate={~p"/raffles/#{raffle}"}>
-          <%= raffle.prize %>
+            {raffle.prize}
           </.link>
         </:col>
         <:col :let={{_dom_id, raffle}} label="Status">
-          <.badge status={raffle.status}/>
+          <.badge status={raffle.status} />
         </:col>
         <:col :let={{_dom_id, raffle}} label="Ticket Price">
-          <%= raffle.ticket_price %>
+          {raffle.ticket_price}
         </:col>
         <:action :let={{_dom_id, raffle}}>
-        <.link navigate={~p"/admin/raffles/#{raffle}/edit"}>
-        Edit
-        </.link>
+          <.link navigate={~p"/admin/raffles/#{raffle}/edit"}>
+            Edit
+          </.link>
         </:action>
-        <:action :let={{_dom_id, raffle}}>
-        <.link phx-click="delete" phx-value-id={raffle.id} data-confirm="Are you sure?">
-        Delete
-        </.link>
+        <:action :let={{dom_id, raffle}}>
+          <.link phx-click={delete_and_hide(dom_id, raffle)} data-confirm="Are you sure?">
+            Delete
+          </.link>
         </:action>
       </.table>
     </div>
@@ -52,7 +65,14 @@ defmodule RaffleyWeb.AdminRaffleLive.Index do
   def handle_event("delete", %{"id" => id}, socket) do
     raffle = Admin.get_raffle!(id)
     {:ok, _} = Admin.delete_raffle(raffle)
-    
+
     {:noreply, stream_delete(socket, :raffles, raffle)}
+  end
+
+  def delete_and_hide(dom_id, raffle) do
+    JS.push("delete",
+      value: %{id: raffle.id}
+    )
+    |> JS.hide(to: "##{dom_id}", transition: "fade-out")
   end
 end
